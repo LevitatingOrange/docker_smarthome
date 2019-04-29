767bea4e2fb383e58af1d99adb5322a86ec943e9
Lennart Vogelsang <lennart@vogelsang.berlin>
2019-04-29 04:21:29 +0200

    fixed unifi psk updater

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
-alias run_rsync="rsync --exclude '.DS_Store/' --exclude 'log/' --exclude 'raspbian.img' -az ./ $1:~/docker_smarthome"
+alias run_rsync="rsync --exclude 'unifi-psk-updater/target' --exclude '.DS_Store/' --exclude 'log/' --exclude 'raspbian.img' -az ./ $1:~/docker_smarthome"
 run_rsync
 #fswatch -o . | while read f; do run_rsync; done
