1073307e85b138b5cd2361627e9458ae24c52e86
Lennart Vogelsang <lennart@vogelsang.berlin>
2021-02-18 21:59:20 +0100

    So much stuff its amazing

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 rsync -ac ./config $1:~/docker_smarthome/
+rsync -ac ./cups_custom_ppd $1:~/docker_smarthome/
 rsync -ac ./docker-compose.yml $1:~/docker_smarthome/docker-compose.yml 
 rsync -ac ./ikea-ota-download.py $1:~/docker_smarthome/ikea-ota-download.py
 
